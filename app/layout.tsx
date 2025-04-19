8d75e83c6e56cbb7eaa256e1849ac5d34d189dd3
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-19 15:02:53 +0100

    update className

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body className={`${inter.className} antialiased`}>
+            <body className={inter.className}>
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="light"
