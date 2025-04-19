4c8dce04050f9ec53fe4bb5782d02769c6bdd1ac
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-19 13:07:16 +0100

    update product.actions

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,9 +1,8 @@
 'use server';
-import {PrismaClient} from '@prisma/client';
 import {convertToPlainObject} from '../utils';
 import {LATEST_PRODUCTS_LIMIT} from '../constants';
+import {prisma} from '@/db/prisma';
 
-const prisma = new PrismaClient();
 // Get latest products
 export async function getLatestProducts() {
     const data = await prisma.product.findMany({
