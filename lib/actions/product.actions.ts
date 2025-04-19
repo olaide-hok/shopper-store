1a958439860645c93a302935b5f849b1c8301989
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-19 09:45:04 +0100

    add getProductBySlug server action

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,14 +3,20 @@ import {PrismaClient} from '@prisma/client';
 import {convertToPlainObject} from '../utils';
 import {LATEST_PRODUCTS_LIMIT} from '../constants';
 
+const prisma = new PrismaClient();
 // Get latest products
 export async function getLatestProducts() {
-    const prisma = new PrismaClient();
-
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
         orderBy: {createdAt: 'desc'},
     });
 
     return convertToPlainObject(data);
+}
+
+// Get single product by it's slug
+export async function getProductBySlug(slug: string) {
+    return await prisma.product.findFirst({
+        where: {slug: slug},
+    });
 }
