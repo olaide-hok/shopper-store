1f39fe5e36d335f62d35c4d23795de1ecb68e41d
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-19 09:03:52 +0100

    update page root

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,15 @@
-export default function Homepage() {
-    return <div className="">Home</div>;
+import ProductList from '@/components/shared/product/product-list';
+import {getLatestProducts} from '@/lib/actions/product.actions';
+
+export default async function Homepage() {
+    const latestProducts = await getLatestProducts();
+    return (
+        <div className="">
+            <ProductList
+                data={latestProducts}
+                title="Newest Arrivals"
+                limit={4}
+            />
+        </div>
+    );
 }
