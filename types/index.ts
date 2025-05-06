586d24e37c4e09a1317a0462c1a641f01d0ebce1
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-06 17:49:23 +0100

    create order item and payment result types

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,10 @@
 import {
     cartItemSchema,
     insertCartSchema,
+    insertOrderItemSchema,
+    insertOrderSchema,
     insertProductSchema,
+    paymentResultSchema,
     shippingAddressSchema,
 } from '@/lib/validators';
 import {z} from 'zod';
@@ -16,3 +19,18 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+    id: string;
+    createdAt: Date;
+    isPaid: boolean;
+    paidAt: Date | null;
+    isDelivered: boolean;
+    deliveredAt: Date | null;
+    orderitems: OrderItem[];
+    user: {name: string; email: string};
+    paymentResult: PaymentResult;
+};
+
+export type PaymentResult = z.infer<typeof paymentResultSchema>;
