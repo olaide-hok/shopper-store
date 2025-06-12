67ac974bfab14ca3ed3857a713fb1165d01d7299
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-06-12 11:11:49 +0100

    ad images config for uploadthing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+                port: '',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
