607e3647683eddb0c1a7b3d6684e976eff62c973
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-06-12 14:55:25 +0100

    create updateUser user action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import {
     shippingAddressSchema,
     signInFormSchema,
     signUpFormSchema,
+    updateUserSchema,
 } from '../validators';
 import {auth, signIn, signOut} from '@/auth';
 import {prisma} from '@/db/prisma';
@@ -231,5 +232,27 @@ export async function deleteUser(id: string) {
             success: false,
             message: formatError(error),
         };
+    }
+}
+
+// Update a user
+export async function updateUser(user: z.infer<typeof updateUserSchema>) {
+    try {
+        await prisma.user.update({
+            where: {id: user.id},
+            data: {
+                name: user.name,
+                role: user.role,
+            },
+        });
+
+        revalidatePath('/admin/users');
+
+        return {
+            success: true,
+            message: 'User updated successfully',
+        };
+    } catch (error) {
+        return {success: false, message: formatError(error)};
     }
 }
