1a49371c7dd6309d938efc314324594eee06a634
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-06-12 14:59:20 +0100

    update isRedirectError import path

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import {isRedirectError} from 'next/dist/client/components/redirect';
+import {isRedirectError} from 'next/dist/client/components/redirect-error';
 import {convertToPlainObject, formatError} from '../utils';
 import {auth} from '@/auth';
 import {getMyCart} from './cart.actions';
