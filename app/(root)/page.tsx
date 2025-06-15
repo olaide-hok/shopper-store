d1f069fd62dd241e54ed20dcefdf52e849d940b2
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-06-15 11:18:52 +0100

    conditionally render ProductCarousel for when featuredProducts exists.
    render view-all-products-button.
    
    render icon-boxez and deal-countdown timer.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,32 @@
 import ProductList from '@/components/shared/product/product-list';
-import {getLatestProducts} from '@/lib/actions/product.actions';
+import {
+    getLatestProducts,
+    getFeaturedProducts,
+} from '@/lib/actions/product.actions';
+import ProductCarousel from '@/components/shared/product/product-carousel';
+import ViewAllProductsButton from '@/components/view-all-products-button';
+import IconBoxes from '@/components/icon-boxes';
+import DealCountdown from '@/components/deal-countdown';
 
-export default async function Homepage() {
+const Homepage = async () => {
     const latestProducts = await getLatestProducts();
+    const featuredProducts = await getFeaturedProducts();
+
     return (
-        <div className="">
+        <>
+            {featuredProducts.length > 0 && (
+                <ProductCarousel data={featuredProducts} />
+            )}
             <ProductList
                 data={latestProducts}
                 title="Newest Arrivals"
                 limit={4}
             />
-        </div>
+            <ViewAllProductsButton />
+            <DealCountdown />
+            <IconBoxes />
+        </>
     );
-}
+};
+
+export default Homepage;
