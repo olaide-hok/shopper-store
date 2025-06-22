cac8a42771f306b077171ade2cc72152ce66707a
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-06-22 16:48:29 +0100

    add userId to Order type definition

--- types/index.ts
+++ types/index.ts
@@ -24,6 +24,7 @@ export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
 export type OrderItem = z.infer<typeof insertOrderItemSchema>;
 export type Order = z.infer<typeof insertOrderSchema> & {
     id: string;
+    userId: string;
     createdAt: Date;
     isPaid: boolean;
     paidAt: Date | null;
