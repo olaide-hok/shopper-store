02fd3fe1cbff39911e1e81d4935ffcd06fac7754
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-06-22 14:41:46 +0100

    remove unused code.
    create AuthResponse type.
    
    type signInWithCredentials with AuthResponse

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -16,14 +16,17 @@ import {ShippingAddress} from '@/types';
 import {z} from 'zod';
 import {PAGE_SIZE} from '../constants';
 import {Prisma} from '@prisma/client';
-// import {Prisma} from '@/lib/generated/prisma';
 import {revalidatePath} from 'next/cache';
 
+type AuthResponse = {
+    success: boolean;
+    message: string;
+};
 // Sign in the user with credentials
 export async function signInWithCredentials(
     prevState: unknown,
     formData: FormData
-) {
+): Promise<AuthResponse> {
     try {
         const user = signInFormSchema.parse({
             email: formData.get('email'),
